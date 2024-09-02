f99ef7888d0a4619fc7a165d0052edf95920d968
omarlkhalil <42020332@hti.edu.eg>
2024-09-02 17:06:09 +0300

    #UPDATE

--- app/src/main/java/com/larryyu/zonak_task/di/HomeModule.kt
+++ app/src/main/java/com/larryyu/zonak_task/di/HomeModule.kt
@@ -1,4 +1,26 @@
 package com.larryyu.zonak_task.di
 
+import com.larryyu.zonak_task.data.remote.NewsEndPoint
+import com.larryyu.zonak_task.data.repoimpl.NewsRepoImplementation
+import com.larryyu.zonak_task.domain.repo.NewsRepo
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object HomeModule {
+
+    @Singleton
+    @Provides
+    fun provideNewsEndPoint(retrofit: Retrofit): NewsEndPoint =
+        retrofit.create(NewsEndPoint::class.java)
+
+    @Singleton
+    @Provides
+    fun provideNewsRepo(api: NewsEndPoint): NewsRepo =
+        NewsRepoImplementation(api)
 }
